bc8abbf567391a1eaed22ecbab93923c2e22d807
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-05 12:08:16 -0300

    inserido o hashmap e scanner

--- src/br/com/zup/DesafioDeModulo.java
+++ src/br/com/zup/DesafioDeModulo.java
@@ -1,8 +1,19 @@
 package br.com.zup;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class DesafioDeModulo {
 
     public static void main(String[] args) {
-	// write your code here
+	/*
+	Crie um programa para gerenciar uma lista de funcionários de uma empresa, cada funcionário tem nome, telefone, email e CPF. No final do programa deve ser exibida a lista de funcionários cadastrados.
+	 */
+        Map<String, String> listaFuncionarios = new HashMap<String, String>();
+        Scanner leitorFuncionarios = new Scanner(System.in);
+
+
+
     }
 }

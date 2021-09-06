4dd952695dc190a15c3f97225a3dc38e115961d2
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-05 23:29:24 -0300

    Tentando colocar a exclusão de usuários

--- src/br/com/zup/DesafioDeModulo.java
+++ src/br/com/zup/DesafioDeModulo.java
@@ -10,27 +10,28 @@ public class DesafioDeModulo {
 	/*
 	Crie um programa para gerenciar uma lista de funcionários de uma empresa, cada funcionário tem nome, telefone, email e CPF. No final do programa deve ser exibida a lista de funcionários cadastrados.
 	 */
+        //HashMap listaFuncionários
         Map<String, String> listaFuncionarios = new HashMap<String, String>();
+        //Inicio do Scanner
         Scanner leitorFuncionarios = new Scanner(System.in);
 
-        //Variaveis
-        //String chaveCadastro = "";
+        //Variáveis
         String nome = "Maria Ferraz da Silva Souza";
         String telefone = "55+61+999-999-999";
         String email = "email@email.com.br";
         String cpf = "761.350.300-35";
-        // String chaveCadastro = leitorFuncionarios.next();
-
+        // Mensagens para o usuário
         System.out.println("-----Sistema de Cadastro de Funcionários-----");
         while (true) {
             //Menu de opções
             System.out.println("Para ---cadastrar--- funcionário, DIGITE 1.");
             System.out.println("Para ---verificar--- funcionários cadastrados, DIGITE 2.");
-            System.out.println("Para ---excluir--- algum funcionario cadastrado, DIGITE 3.");
+            System.out.println("Para ---excluir--- algum funcionário cadastrado, DIGITE 3.");
             System.out.println("Para ---encerrar--- o programa, DIGITE 4.");
-            int opcaoEscolhida = leitorFuncionarios.nextInt();
+            int opcaoEscolhida = leitorFuncionarios.nextInt(); //opção escolhida pelo usuário
             leitorFuncionarios.nextLine();
             // Condições do programa
+            // Cadastro dos funcionários
             if (opcaoEscolhida == 1) {
                 System.out.println("Insira o nome do funcionario:");
                 nome = leitorFuncionarios.nextLine();
@@ -40,26 +41,37 @@ public class DesafioDeModulo {
                 email = leitorFuncionarios.nextLine();
                 System.out.println("Insira o CPF do funcionario:");
                 cpf = leitorFuncionarios.nextLine();
-                System.out.println("Cadastro realizado com sucesso:");
+                System.out.println("Cadastro realizado com sucesso!!!");
+            } while (listaFuncionarios.containsKey(cpf)) {
+                System.out.println("Usuário já consta cadastrado! Digite um novo CPF: ");
+                cpf = leitorFuncionarios.nextLine();
             }
+            //verifica se tem cpf igual na base de dados
+            /*if (listaFuncionarios.containsValue(cpf)) {
+                System.out.println("Usuário já consta cadastrado!");
+            }*/
+            //lista de funcionários cadastrados
             if (opcaoEscolhida == 2) {
-                System.out.println("Funcionários já cadastrados: ");
+                System.out.println("Lista de funcionários já cadastrados: ");
                 listaFuncionarios.put("CPF: " + cpf, " | " + "Nome: " + nome + " | " + "Telefone: " + telefone + " | " + "E-mail: " + email + "\n");
                 System.out.println(listaFuncionarios);
-                System.out.print("\n");
-            }
-
-            if (listaFuncionarios.containsValue(cpf)) {
-                System.out.println("Usuário já consta cadastrado!");
+                System.out.print("\n"); //saltar uma linha
+            } //remove funcionário do cadastro
+            if (opcaoEscolhida == 3) {
+                System.out.println("Digite o CPF do funcionário para excluir o cadastro:");
+                cpf = leitorFuncionarios.next();
+                listaFuncionarios.remove(cpf);
+                System.out.println("Funcionário " + cpf + " removido do cadastro com sucesso!");
+            }//entrada inválida
+            if (opcaoEscolhida > 4 || opcaoEscolhida < 1) {
+                System.out.println("Opção de entrada inválida. Verifique novamente!");//finaliza o programa
             }
             if (opcaoEscolhida == 4) {
                 break;
 
-
             }
-
         }
     }
-
 }
+
 
